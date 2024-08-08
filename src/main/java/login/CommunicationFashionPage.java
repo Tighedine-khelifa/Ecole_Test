d29ee6607f7b7ee627f54d90834f6e7ae92d6190
Tighedine Khelifa <tighedinekhelifa@gmail.com>
2024-08-08 10:37:23 +0200

    Tester le iframe de la page fashion 1

--- src/main/java/login/CommunicationFashionPage.java
+++ src/main/java/login/CommunicationFashionPage.java
@@ -14,6 +14,7 @@ import java.time.Duration;
 public class CommunicationFashionPage {
 
 
+
     WebDriver driver;
     public CommunicationFashionPage(WebDriver driver){
         this.driver = driver;
